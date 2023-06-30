0d147f98d265e8317fedfcd4cb74e808319f8373
Jan <pcerservices@gmail.com>
2023-06-30 15:31:18 +0200

    Signup html page created and sending post data to server

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
-
 const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function() {
     console.log("Listening on port 3000");
 })
 
 app.get("/", function(req, res) {
-    res.send("Express working");
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res) {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    console.log(`Name: ${firstName} ${lastName} with email ${email}`);
 })
