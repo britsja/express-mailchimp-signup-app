94a12d6af3e1f3ca5d2e3dc304bd4851aff0ea06
Jan <pcerservices@gmail.com>
2023-07-03 13:02:21 +0200

    Create success and failure pages for signup and routes to redirect back home

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
 })
 
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
+app.post("/success", function(req, res) {
+    res.redirect("/");
+})
+
 app.post("/", function(req, res) {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
@@ -52,8 +60,13 @@ app.post("/", function(req, res) {
     //Using node http.request https://nodejs.org/api/http.html#requestmethod
     const request = https.request(url, options, function(response) {
         response.on("data", function(data) {
-            console.log(JSON.parse(data));
+            // console.log(JSON.parse(data));
         })
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {            
+            res.sendFile(__dirname + "/failure.html");
+        }
     })
 
     request.write(jsonData);
